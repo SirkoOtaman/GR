d558ee9f6f19b79cda6f17456de2a6bea7e72abe
SirkoOtaman <sergij41@gmail.com>
2013-07-11 22:23:02 +0300

    LogIn-Out

--- GRecruiting/src/edu/grecruiting/controllers/session/LogInOutController.java
+++ GRecruiting/src/edu/grecruiting/controllers/session/LogInOutController.java
@@ -10,6 +10,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import edu.grecruiting.model.student.StudentEntity;
 import edu.grecruiting.model.user.UserEntity;
 import edu.grecruiting.model.user.UserEntityManager;
 
@@ -19,13 +20,13 @@ import edu.grecruiting.model.user.UserEntityManager;
 @WebServlet("/LogInOutController")
 public class LogInOutController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public LogInOutController() {
-        super();
-    }
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public LogInOutController() {
+		super();
+	}
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
@@ -38,13 +39,20 @@ public class LogInOutController extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		String login = request.getParameter("login");
-		String pass = request.getParameter("pass");
-		UserEntity currentUser = UserEntityManager.getByLoginPassword(login, pass);
-		HttpSession session = request.getSession(true);
-		session.setAttribute("USER", currentUser.getType());
-		
-		
+		request.setCharacterEncoding("UTF-8");
+		if(request.getParameter("submitLogin")!=null){
+			String login = request.getParameter("login");
+			String pass = request.getParameter("pass");
+			UserEntity currentUser = UserEntityManager.getByLoginPassword(login, pass);
+			if(currentUser!=null){
+				HttpSession session = request.getSession(true);
+				session.setAttribute("USER", currentUser);
+			}
+		}
+		if(request.getParameter("submitLogOut")!=null){
+			request.getSession().invalidate();
+		}
+		response.sendRedirect("/GRecruiting/student/student.jsp");
 	}
 
 }
